b482c49c9b01fef7637f942ca133c0882bc0b601
iman <dev.imansabet@gmail.com>
2024-12-09 12:54:53 +0330

    Add Generic Interface - implement Generic Repository - Add data base context class - Add Exception class - inject Service & Config Db Connection

---
+++ eCommerceApp.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,34 @@
+﻿using eCommerceApp.Domain.Entities;
+using eCommerceApp.Domain.Interfaces;
+using eCommerceApp.Infrastructure.Data;
+using eCommerceApp.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerceApp.Infrastructure.DependencyInjection;
+
+public static class ServiceContainer
+{
+    public static IServiceCollection AddInfrastructureService
+        (this IServiceCollection  services,IConfiguration configuration)
+    
+    {
+        string connectionString = "Default";
+        services.AddDbContext<AppDbContext>
+            (option =>
+                option.UseSqlServer(configuration.GetConnectionString(connectionString),
+                sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(ServiceContainer).Assembly.FullName);
+                    sqlOptions.EnableRetryOnFailure();
+                }),
+                ServiceLifetime.Scoped
+           ); ;
+
+        services.AddScoped<IGeneric<Product>, GenericRepository<Product>>();
+        services.AddScoped<IGeneric<Category>, GenericRepository<Category>>();
+
+        return services;
+    }
+}
